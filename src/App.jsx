7f867f8876bce3c08dc9b5bc938a7c79f0da96a8
Alkeops <m_adan2012@outlook.com>
2023-07-03 17:44:31 -0500

    Sin

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,7 @@ const CreateBook = ({ addBook }) => {
     </form>
   );
 };
-const Container = ({ children }) => {
-  return <div>{children}</div>;
-};
+
 function App() {
   const [books, setBooks] = useState([
     "Dracula",
@@ -40,50 +38,10 @@ function App() {
     "Cien años de soledad",
     "Los miserables",
   ]);
-  const ref = useRef(0);
 
-  /*  const [book, setBook] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addBook(book);
-    setBook("");
-  };
-  const handleChange = (e) => {
-    setBook(e.target.value);
-  }; */
-  const test = () =>{
-    React.createElement("div", null, "Hola mundo");
-  };
-    return (
+  return (
     <div>
-     
-      {/* <form onSubmit={handleSubmit}>
-        <input value={book} onChange={handleChange} />
-        <button disabled={!book} type="submit">
-          Añadir libro
-        </button>
-      </form> */}
       <CreateBook addBook={(book) => setBooks([...books, book])} />
-      <Container>Children</Container>
-      <Container
-        children={
-          <div>
-            <h1>algo asi</h1>
-          </div>
-        }
-      />
-      {Container({
-        children: "Hola mundo",
-      })}
-      {ref.current}
-      <button
-        onClick={() => {
-          console.log("Click");
-          ref.current++;
-        }}
-      >
-        Incrementar
-      </button>
       <h2>Best Sellers</h2>
       <div className="books">
         {books.map((element, idx) => (
